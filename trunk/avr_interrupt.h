b8c325f1d0ba4f17e8f012292641761d56eb0a49
m0slevin <m0slevin@d6115ad4-925e-42b1-b8ed-db49d7990a81>
2014-09-20 01:42:02 +0000

    Adding preliminary interrupt support, plus tie-ins with the UART plugin.  Untested.

---
+++ trunk/avr_interrupt.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+ *     (     (                      (     |
+ *    )\ )  )\ )    (              )\ )   |
+ *   (()/( (()/(    )\     (   (  (()/(   | -- [ Funkenstein ] -------------
+ *    /(_)) /(_))((((_)(   )\  )\  /(_))  | -- [ Litle ] -------------------
+ *   (_))_|(_))   )\ _ )\ ((_)((_)(_))    | -- [ AVR ] ---------------------
+ *   | |_  | |    (_)_\(_)\ \ / / | _ \   | -- [ Virtual ] -----------------
+ *   | __| | |__   / _ \   \ V /  |   /   | -- [ Runtime ] -----------------
+ *   |_|   |____| /_/ \_\   \_/   |_|_\   |
+ *                                        | "Yeah, it does Arduino..."
+ * ---------------------------------------+----------------------------------
+ * (c) Copyright 2014, Funkenstein Software Consulting, All rights reserved
+ *     See license.txt for details
+ ****************************************************************************/
+/*!
+  \file  avr_interrupt.h
+
+  \brief CPU Interrupt management
+*/
+
+#ifndef __AVR_INTERRUPT_H__
+#define __AVR_INTERRUPT_H__
+
+#include <stdint.h>
+#include "emu_config.h"
+#include "avr_cpu.h"
+
+//---------------------------------------------------------------------------
+/*!
+ * \brief AVR_InterruptCandidate
+ *
+ * Given an existing interrupt candidate, determine if the selected interrupt
+ * vector is of highier priority.  If higher priority, update the candidate.
+ *
+ * \param pstCPU_ Pointer to the CPU object to run the interrupt
+ * \param u8Vector_ - Candidate interrupt vector.
+ */
+void AVR_InterruptCandidate( AVR_CPU *pstCPU_, uint8_t u8Vector_ );
+
+//---------------------------------------------------------------------------
+/*!
+ * \brief AVR_Interrupt
+ *
+ * Entrypoint for CPU interrupts.  Stop executing the currently-executing
+ * code, push the current PC to the stack, disable interrupts, and resume
+ * execution at the new location specified in the vector table.
+ *
+ * \param pstCPU_ Pointer to the CPU object to run the interrupt
+ */
+void AVR_Interrupt( AVR_CPU *pstCPU_ );
+
+#endif //__AVR_INTERRUPT_H__
