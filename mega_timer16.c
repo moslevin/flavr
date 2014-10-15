5a6d05b9a16dcbef66d0559efa9a58cdf0bdfb85
m0slevin <m0slevin@d6115ad4-925e-42b1-b8ed-db49d7990a81>
2014-10-15 03:50:13 +0000

    Fixed off-by-one error when reloading timer prescalar
    git-svn-id: svn+ssh://svn.code.sf.net/p/flavr/code/trunk@18 d6115ad4-925e-42b1-b8ed-db49d7990a81

--- mega_timer16.c
+++ mega_timer16.c
@@ -400,7 +400,8 @@ static void Timer16_Clock(void *context_, struct _AVR_CPU *pstCPU_)
             //DEBUG_PRINT(" %d ticks remain\n", u16DivRemain);
             u16DivRemain--;
         }
-        else
+
+        if (!u16DivRemain)
         {
             // clock-divider count hits zero, reset and trigger an update.
             //DEBUG_PRINT(" expire and reset\n");
